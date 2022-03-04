d53154a75a25ab784b8d0d5d4dbf09b6672fadf8
Podbeltsev Aleksei <Podbelcev_aleksei@mail.ru>
2022-03-04 16:15:49 +0300

    Using Profile annotation

--- src/main/java/com/springframework/springdi/SpringDiApplication.java
+++ src/main/java/com/springframework/springdi/SpringDiApplication.java
@@ -1,9 +1,6 @@
 package com.springframework.springdi;
 
-import com.springframework.springdi.controller.ConstructorInjectedController;
-import com.springframework.springdi.controller.MyController;
-import com.springframework.springdi.controller.PropertyInjectedController;
-import com.springframework.springdi.controller.SetterInjectedController;
+import com.springframework.springdi.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SpringDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
 
         MyController myController = (MyController) ctx.getBean("myController");
 
