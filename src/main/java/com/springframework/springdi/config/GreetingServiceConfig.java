e3e7f87a9b2884403515a7b74961aff615c0660a
Podbeltsev Aleksei <Podbelcev_aleksei@mail.ru>
2022-03-09 14:32:11 +0300

    DI in Java Configuration

--- src/main/java/com/springframework/springdi/config/GreetingServiceConfig.java
+++ src/main/java/com/springframework/springdi/config/GreetingServiceConfig.java
@@ -1,5 +1,7 @@
 package com.springframework.springdi.config;
 
+import com.springframework.springdi.repositories.EnglishGreetingRepository;
+import com.springframework.springdi.repositories.EnglishGreetingRepositoryImpl;
 import com.springframework.springdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,10 +32,15 @@ public class GreetingServiceConfig {
         return new PrimaryGreetingService();
     }
 
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
+
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService() {
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     @Profile({"ES", "default"})
--- src/main/java/com/springframework/springdi/services/I18nEnglishGreetingService.java
+++ src/main/java/com/springframework/springdi/services/I18nEnglishGreetingService.java
@@ -1,12 +1,19 @@
 package com.springframework.springdi.services;
 
+import com.springframework.springdi.repositories.EnglishGreetingRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 
 public class I18nEnglishGreetingService implements GreetingService{
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hello World - EN";
+        return englishGreetingRepository.getGreeting() + " - EN";
     }
 }
