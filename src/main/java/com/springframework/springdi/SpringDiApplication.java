d08bbac07764ad4fb5228bb2e937cd46a7f65a15
Podbeltsev Aleksei <Podbelcev_aleksei@mail.ru>
2022-03-12 00:49:37 +0300

    Demonstrating of bean scope difference

--- src/main/java/com/springframework/springdi/SpringDiApplication.java
+++ src/main/java/com/springframework/springdi/SpringDiApplication.java
@@ -1,6 +1,8 @@
 package com.springframework.springdi;
 
 import com.springframework.springdi.controller.*;
+import com.springframework.springdi.services.PrototypeBean;
+import com.springframework.springdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -41,6 +43,18 @@ public class SpringDiApplication {
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 
         System.out.println(constructorInjectedController.getGreeting());
+
+        System.out.println("----- Bean scope -------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
+
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
+
 
     }
 
